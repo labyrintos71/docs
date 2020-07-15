697851abbbcf0ba109b35aa69faf20a050285d52
labyrintos71 <labyrintos71@aiaracorp.com>
2020-07-15 10:19:28 +0900

    fix config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,17 @@
 const { description } = require('../../package')
 
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
+  base: '/docs/',
   title: 'Vuepress Docs Boilerplate',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
   description: description,
-
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
 
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
-    repo: '',
     editLinks: false,
-    docsDir: '',
-    editLinkText: '',
     lastUpdated: false,
     nav: [
       {
